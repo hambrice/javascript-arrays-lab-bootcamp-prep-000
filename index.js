e8a262414d32eaec491fe914902295a2ae766fe1
Brad Hambrice <hambrice@uab.edu>
2017-11-19 02:57:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens
+}
+function appendKitten(name) {
+ var kittensNew = [...kittens, name]
+  return kittensNew
+}
+function prependKitten(name) {
+  var kittensNew = [name, ...kittens]
+  return kittensNew
+}
+function removeLastKitten() {
+  var kittensNew = kittens.splice(2,1)
+  return kittensNew
+}
